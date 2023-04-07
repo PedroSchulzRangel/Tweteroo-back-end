f85647133af400688f1526b86d7fff31c7076271
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-04-07 20:34:13 -0300

    Requisito: POST/tweets

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const PORT = 5000;
 
 const users = [];
 
+const tweets = [];
+
+let username="";
+
+let avatar =""; 
+
 app.use(cors());
 
 app.use(express.json());
@@ -14,7 +20,9 @@ app.use(express.json());
 
 app.post("/sign-up", (req, res) => {
     
-    const {username, avatar} = req.body;
+    username = req.body.username;
+
+    avatar = req.body.avatar;
 
     if(!username || !avatar){
         return res.status(422).send("Preencha todos os campos de login");
@@ -23,6 +31,28 @@ app.post("/sign-up", (req, res) => {
     users.push({username, avatar});
    
     res.send("OK");
+});
+
+app.post("/tweets", (req,res) => {
+    
+    const isRegistered = users.find((user) => user.username === username);
+
+    if(!isRegistered){
+        return res.status(401).send("UNAUTHORIZED");
+    }
+    
+    username = req.body.username;
+    
+    const tweet = req.body.tweet;
+
+    if(!tweet){
+        return res.status(422).send("Preencha o nome do tweet");
+    }
+    
+    tweets.push({username, tweet});
+
+    res.send("OK");
+
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
